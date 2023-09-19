4f88a6f8b839733d9b5493ef5d201cc2d513b550
ricocanuto <ricardomoraescanuto@gmail.com>
2023-09-19 08:00:59 -0300

    Random Project

---
+++ random.js
@@ -0,0 +1,15 @@
+function sortearNumero(){
+    
+    const min=Math.floor(document.querySelector('.input-min').value)
+    const max=Math.ceil(document.querySelector('.input-max').value)
+
+    const result=Math.floor(Math.random()*(max-min+1))+min;
+
+    console.log(min,max)
+
+    const numeroSorteado = document.getElementById("resultadoSorteio")
+    
+    numeroSorteado.textContent = "Número sorteado: " + result;
+
+    
+}
